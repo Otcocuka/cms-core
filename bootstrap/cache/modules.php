d2df0b9504d56a532f90d0a0ce5cb075bc8db220
Otcocuka <d.ipharziev@gmail.com>
2025-12-01 07:56:21 +0400

    Initial CMS Core setup

---
+++ bootstrap/cache/modules.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Content\\Providers\\ContentServiceProvider',
+    1 => 'Modules\\Forms\\Providers\\FormsServiceProvider',
+    2 => 'Modules\\Media\\Providers\\MediaServiceProvider',
+    3 => 'Modules\\SEO\\Providers\\SEOServiceProvider',
+    4 => 'Modules\\Settings\\Providers\\SettingsServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Content\\Providers\\ContentServiceProvider',
+    1 => 'Modules\\Forms\\Providers\\FormsServiceProvider',
+    2 => 'Modules\\Media\\Providers\\MediaServiceProvider',
+    3 => 'Modules\\SEO\\Providers\\SEOServiceProvider',
+    4 => 'Modules\\Settings\\Providers\\SettingsServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,94 @@
+<?php return array (
+  'laravel/breeze' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Breeze\\BreezeServiceProvider',
+    ),
+  ),
+  'laravel/pail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Pail\\PailServiceProvider',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'livewire/livewire' => 
+  array (
+    'aliases' => 
+    array (
+      'Livewire' => 'Livewire\\Livewire',
+    ),
+    'providers' => 
+    array (
+      0 => 'Livewire\\LivewireServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'nwidart/laravel-modules' => 
+  array (
+    'aliases' => 
+    array (
+      'Module' => 'Nwidart\\Modules\\Facades\\Module',
+    ),
+    'providers' => 
+    array (
+      0 => 'Nwidart\\Modules\\LaravelModulesServiceProvider',
+    ),
+  ),
+  'spatie/laravel-medialibrary' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\MediaLibrary\\MediaLibraryServiceProvider',
+    ),
+  ),
+  'spatie/laravel-permission' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Permission\\PermissionServiceProvider',
+    ),
+  ),
+  'spatie/laravel-translatable' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Translatable\\TranslatableServiceProvider',
+    ),
+  ),
+);
